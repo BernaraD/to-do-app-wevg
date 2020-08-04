b7bbe15c547880e5f32694cc15d78545f58d4159
Bernara Dzhaparova <57506492+Betty8Bee@users.noreply.github.com>
2020-08-04 18:43:27 -0400

    Updates

---
+++ precache-manifest.04122e96356f9865ed96c848a7a8d49a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7434d163b42e9e2c6acac4c9d0524fd8",
+    "url": "/to-do-app-wevg/index.html"
+  },
+  {
+    "revision": "aa72b5b2e5298c5b7169",
+    "url": "/to-do-app-wevg/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "265bdb2f54311bf39759",
+    "url": "/to-do-app-wevg/static/css/main.fed69db3.chunk.css"
+  },
+  {
+    "revision": "aa72b5b2e5298c5b7169",
+    "url": "/to-do-app-wevg/static/js/2.41215782.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app-wevg/static/js/2.41215782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265bdb2f54311bf39759",
+    "url": "/to-do-app-wevg/static/js/main.26686888.chunk.js"
+  },
+  {
+    "revision": "64caf7910837b0b94901",
+    "url": "/to-do-app-wevg/static/js/runtime-main.687801f4.js"
+  }
+]);
